9cc25970d2b230c697273c301d4a11338c5ce99e
GDO <2992362310@qq.com>
2023-12-15 15:44:33 +0800

    实践

--- packages/fe-world/src/views/FrontJob/index.vue
+++ packages/fe-world/src/views/FrontJob/index.vue
@@ -1,3 +1,73 @@
 <template>
-  <div>前端面试</div>
+  <section class="pl-10">
+    <div>
+      <h1>前端开发环境搭建</h1>
+      <ul>
+        <li>
+          <a href="https://nvm.uihtm.com/">nvm(node.js版本管理)</a>
+        </li>
+        <li>
+          <a href="https://git-scm.com/about">git</a>
+        </li>
+        <li>
+          <a href="https://www.pnpm.cn/">pnpm</a>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>vue项目搭建</h2>
+      <ul>
+        <li>
+          <a href="https://cn.vuejs.org/guide/quick-start.html">vue@latest</a>
+        </li>
+        <li>
+          <a href="https://cn.vitejs.dev/guide/features.html#postcss">vite</a>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>react项目搭建</h2>
+      <ul>
+        <li>
+          <a href="https://cn.vitejs.dev/guide/#scaffolding-your-first-vite-project">vite react</a>
+        </li>
+        <li>
+          <a href="https://create-react-app.bootcss.com/">create-react-app</a>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>开发规范工具</h2>
+      <ul>
+        <li><a href="https://www.typescriptlang.org/zh/">typescript</a></li>
+        <li><a href="https://eslint.nodejs.cn/">eslint</a></li>
+        <li><a href="https://www.prettier.cn/">prettier</a></li>
+        <li><a href="https://www.stylelint.com.cn/">stylelint</a></li>
+        <li>
+          <a href="https://mp.weixin.qq.com/s/0q13PZ6vNxrbrpuxQxxt-g">
+            开启高效前端规范开发之旅！ESLint + Prettier + husky +Commitizen
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>小程序</h2>
+    </div>
+
+    <div>
+      <h2>桌面端</h2>
+    </div>
+
+    <div>
+      <h2>App</h2>
+    </div>
+
+    <div>
+      <h2>monorepo</h2>
+    </div>
+  </section>
 </template>
