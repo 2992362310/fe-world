955683b22333bc8b67e95b3e24d082926eb2f642
GDO <2992362310@qq.com>
2023-12-18 15:00:36 +0800

    面试

--- packages/fe-world/src/views/FrontExtend/index.vue
+++ packages/fe-world/src/views/FrontExtend/index.vue
@@ -2,15 +2,18 @@
 const list = [
   {
     name: '算法',
-    description: 'hello-算法'
+    description: 'hello-算法',
+    url: 'https://www.hello-algo.com/'
   },
   {
     name: '设计模式',
-    description: '设计模式'
+    description: '设计模式',
+    url: 'https://juejin.cn/post/6994464022628139045'
   },
   {
     name: '游戏开发',
-    description: 'coco-creator'
+    description: 'coco-creator',
+    url: 'https://cocos.com/creator'
   },
   {
     name: 'nodejs开发',
@@ -21,12 +24,14 @@ const list = [
     description: 'rust、go'
   },
   {
-    name: 'web componnet',
-    description: 'web componnet'
+    name: 'Web Component',
+    description: 'web componnet',
+    url: 'https://developer.mozilla.org/zh-CN/docs/Web/API/Web_components'
   },
   {
     name: 'web container',
-    description: 'web container'
+    description: 'web container',
+    url: 'https://webcontainers.io/'
   }
 ]
 </script>
@@ -35,7 +40,7 @@ const list = [
   <ul class="pl-10">
     <li v-for="item in list" :key="item.name">
       <h2>{{ item.name }}</h2>
-      <p>{{ item.description }}</p>
+      <a :href="item.url" target="_blank">{{ item.description }}</a>
     </li>
   </ul>
 </template>
--- packages/fe-world/src/views/FrontInterview/index.vue
+++ packages/fe-world/src/views/FrontInterview/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>前端实践</div>
+  <ul>
+    <li>
+      <a href="https://zhuanlan.zhihu.com/p/605933425">2023前端面试必备个人总结</a>
+    </li>
+    <li>
+      <a href="https://juejin.cn/post/7218136070738690103">2023年前端常见面试题（附答案）</a>
+    </li>
+    <li>
+      <a href="https://juejin.cn/post/7208005892313579576#heading-0">
+        2023高频前端面试题合集之Vue（上篇）
+      </a>
+    </li>
+    <li>vue</li>
+    <li>react</li>
+    <li>http</li>
+    <li>video</li>
+    <li>笔试--数组去重、promise.all、最大请求</li>
+    <li>vue渲染流程、react渲染流程</li>
+    <li>ofa.js</li>
+    <li>性能优化</li>
+  </ul>
 </template>
